0fc03add4862f302cfcae437dfedf6cb5ac82c19
angtorcal <angela.torcal93@gmail.com>
2023-05-07 13:17:48 +0200

    Zoom y color

--- Proyectos/Esfera/sketch.js
+++ Proyectos/Esfera/sketch.js
@@ -1,12 +1,23 @@
 let angle = 0;
+let colorAngle = 0;
+let sphereSize = 50;
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(1000, 1000, WEBGL);
 }
 
 function draw() {
   background(200);
   rotateY(angle);
-  sphere(50);
+  fill(getColor());
+  sphere(sphereSize);
   angle += 0.01;
+  sphereSize += 0.1;
+}
+
+function getColor() {
+  colorMode(HSB, 360, 100, 100);
+  let c = color(colorAngle % 360, 100, 100);
+  colorAngle += 1;
+  return c;
 }
