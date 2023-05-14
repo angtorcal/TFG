e3796e942baaa4501380c10d5ff8edd1feca8801
angtorcal <angela.torcal93@gmail.com>
2023-05-15 01:09:49 +0200

    Actualizar 4Rayas

--- Proyectos/conect-4/sketch.js
+++ Proyectos/conect-4/sketch.js
@@ -8,9 +8,15 @@ let jugador = 1;
 let posicionJugador;
 let victoria = 0;
 
+let textoInicio = true;
+let spacePressed = false;
+let stopped= false;
+let seconds;
+let startTime;
+let elapsedTime;
+
 function setup() {
   createCanvas(columnas * anchoCelda, filas * anchoCelda + anchoCelda);
-  
 }
 
 function haGanado() {
@@ -31,7 +37,7 @@ function haGanado() {
       }
     }
   }
-  
+
   // Prueba vertical
   for (let j = 0; j <= filas - 4; j++) {
     for (let i = 0; i < columnas; i++) {
@@ -49,7 +55,7 @@ function haGanado() {
       }
     }
   }
-  
+
   // Prueba diagonal
   for (let j = 0; j <= filas - 4; j++) {
     for (let i = 0; i <= columnas - 4; i++) {
@@ -67,7 +73,7 @@ function haGanado() {
       }
     }
   }
-  
+
   // Prueba antidiagonal
   for (let j = 0; j <= filas - 4; j++) {
     for (let i = 4; i < columnas; i++) {
@@ -85,15 +91,14 @@ function haGanado() {
       }
     }
   }
-  
+
   return false;
 }
 
 function draw() {
-  background(190, 229, 238);
-  
+  background(0, 58, 255);
   posicionJugador = floor(mouseX / anchoCelda)
-  
+
   stroke(0);
   fill(255);
   rect(-1, -1, width + 2, anchoCelda);
@@ -101,82 +106,128 @@ function draw() {
     for (let i = 0; i < columnas; i++) {
       fill(255);
       if (tablero[j][i] == 1) {
-        fill(150, 145, 237);
+        fill(255, 0, 62);
       } else if (tablero[j][i] == 2) {
-        fill(242, 172, 235);
+        fill(255, 197, 0);
       }
-      ellipse(i*anchoCelda + anchoCelda/2, j*anchoCelda + 3*anchoCelda/2, anchoCirculo);
+      ellipse(i * anchoCelda + anchoCelda / 2, j * anchoCelda + 3 * anchoCelda / 2, anchoCirculo);
     }
   }
-  
+
   stroke(102, 102, 0);
   for (let x = anchoCelda; x < width; x += anchoCelda) {
     line(x, anchoCelda, x, height);
   }
-  
+
   stroke(0);
   if (jugador == 1) {
-    fill(150, 145, 237);
-  } else if (jugador  == 2) {
-    fill(242, 172, 235);
+    fill(255, 0, 62);
+  } else if (jugador == 2) {
+    fill(255, 197, 0);
   }
-  ellipse((posicionJugador + 0.5) * anchoCelda, anchoCelda/2, anchoCirculo);
-  
-  if (victoria != 0) {
-    noStroke();
-    fill(0);
-    if (victoria == 1) {
-      fill(150, 145, 237);
-    } else if (victoria  == 2) {
-      fill(242, 172, 235);
-    }
+  ellipse((posicionJugador + 0.5) * anchoCelda, anchoCelda / 2, anchoCirculo);
+  noStroke();
+
+  if (textoInicio) {
     textAlign(CENTER, CENTER);
-    textSize(64);
-    if (victoria == 4) {
-      text("¡Game Over!", width/2, anchoCelda/2);
-    } else if (victoria == 3) {
-      text("PARTIDO EMPATADO.", width/2, anchoCelda/2);
+    textStyle(BOLD);
+    stroke(88, 0, 138);
+    strokeWeight(4);
+
+    fill(12, 152, 255);
+    textSize(50);
+    text("NUEVA PARTIDA", width / 2, (height / 2) - 40);
+
+    fill(255, 0, 120);
+    textSize(30);
+    text("\nPULSA ESPACIO PARA EMPEZAR", width / 2, (height / 2) + 70);
+
+    strokeWeight(0);
+    noStroke();
+  }
+  if (!stopped) {
+  // calcular el tiempo transcurrido desde el inicio
+  elapsedTime = millis() - startTime;
+  // convertir el tiempo transcurrido a segundos
+  seconds = Math.floor(elapsedTime / 1000);
+  fill(12, 152, 255);
+  textSize(20);
+  textStyle(BOLD);
+  text(`${isNaN(seconds) ? 'TIEMPO: 0' : 'TIEMPO:' + seconds}`, 100, 30);
+}
+  if (victoria != 0) {
+    stopped=true;
+    textAlign(CENTER, CENTER);
+    textStyle(BOLD);
+    stroke(255);
+    strokeWeight(5);
+    fill(12, 152, 255);
+    textSize(30);
+    if (victoria == 3) {
+      text("PARTIDO EMPATADO!", width / 2, anchoCelda / 2);
+    } else if (victoria == 4) {
+      text("PARTIDO TERMINADO!", width / 2, anchoCelda / 2);
     } else {
-      text(`${victoria > 1 ? 'ROSA' : 'LILA'} WON!`, width/2, anchoCelda/2);
+      text(`${victoria > 1 ? 'JUGADOR AMARILLO' : 'JUGADOR ROJO'} GANADOR!`, width / 2, anchoCelda / 2)
+      fill(255, 0, 120);
+      textSize(20);
+      text("TIEMPO TOTAL: " + seconds, width / 2, (anchoCelda / 2) + 35);
+
     }
-    noLoop();
+    noStroke();
+    strokeWeight(0);
+  }
+
+}
+
+function keyPressed() {
+
+  if (key === ' ') {
+    if (textoInicio) {
+      textoInicio = !textoInicio;
+      // iniciar el cronómetro al presionar la tecla de espacio
+      startTime = millis();
+      spacePressed = true;
+    }
   }
 }
 
 function mousePressed() {
-  if (tablero[0][posicionJugador] != 0) {
-    victoria = 4;
-  }
-  
-  tablero[0][posicionJugador] = jugador;
-  let i = 0;
-  while (true) {
-    if (i >= filas-1) {
-      break;
+  if (spacePressed) {
+    if (tablero[0][posicionJugador] != 0) {
+      victoria = 4;
     }
-    if (tablero[i+1][posicionJugador] != 0) {
-      break;
+
+    tablero[0][posicionJugador] = jugador;
+    let i = 0;
+    while (true) {
+      if (i >= filas - 1) {
+        break;
+      }
+      if (tablero[i + 1][posicionJugador] != 0) {
+        break;
+      }
+      [tablero[i + 1][posicionJugador], tablero[i][posicionJugador]] = [tablero[i][posicionJugador], tablero[i + 1][posicionJugador]];
+      i++;
     }
-    [tablero[i+1][posicionJugador], tablero[i][posicionJugador]] = [tablero[i][posicionJugador], tablero[i+1][posicionJugador]];
-    i++;
-  }
-  
-  if (haGanado()) {
-    victoria = jugador;
-  }
-  
-  let empate = true;
-  for (let j = 0; j < filas; j++) outer: {
-    for (let i = 0; i < columnas; i++) {
-      if (tablero[j][i] == 0) {
-        empate = false;
+
+    if (haGanado()) {
+      victoria = jugador;
+    }
+
+    let empate = true;
+    for (let j = 0; j < filas; j++) outer: {
+      for (let i = 0; i < columnas; i++) {
+        if (tablero[j][i] == 0) {
+          empate = false;
+        }
       }
     }
+
+    if (empate) {
+      victoria = 3;
+    }
+
+    jugador = 3 - jugador;
   }
-  
-  if (empate) {
-    victoria = 3;
-  }
-  
-  jugador = 3 - jugador;
 }
